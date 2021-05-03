973b8cdacc6d52d9839eb5631235c0ce22887d0e
atsushiokumura-oss <81137612+atsushiokumura-oss@users.noreply.github.com>
2021-05-03 21:02:15 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = -12;
+    private float deadLine = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (this.speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy (gameObject);
+        }
+        GetComponent<AudioSource> ().volume = 0;
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        GetComponent<AudioSource> ().volume = (other.gameObject.tag == "CubePrefabTag" || other.gameObject.tag == "groundTag") ? 1 : 0;
+    }
+}
